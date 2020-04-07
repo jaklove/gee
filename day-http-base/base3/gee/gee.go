ee633baf6caf324ac28ab4d1053130fd59b5743d
1004330995 <1004330995@qq.com>
2020-04-08 00:18:32 +0800

    手工

---
+++ day-http-base/base3/gee/gee.go
@@ -0,0 +1,52 @@
+package gee
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+//添加路由
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+//开启当前框架入门
+func(engine *Engine) Run(addr string)error {
+	err := http.ListenAndServe(addr,engine)
+	if err != nil{
+		return  err
+		panic("开启监听服务出错")
+	}
+	return  nil
+}
+
+//接管http请求
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := r.Method + "-" + r.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, r)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", r.URL)
+	}
+}
