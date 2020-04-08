221839093f0ae96b4ba1f10d0be438fb252593bd
1004330995 <1004330995@qq.com>
2020-04-09 01:24:40 +0800

    '生命周期详解'

--- day-router/main.go
+++ day-router/main.go
@@ -28,5 +28,13 @@ func main() {
 	r.Run(":9999")
 }
 
+//1.gee包中包含添加 addRoute注册对路由 =》 处理的funcHandler ,以及开始监听服务,并且实现ServeHTTP方法，接管请求的操作,其中engine包含router结构体
+//2.其中router中包含 roots map和handlers map,其中key都是以 method+'-'+pattern，其中roots中存放着关于node结构体的节点
+//3.在trie.go中定义node的结构体，具体有insert、search、matchChild、matchChildren
+
+
+//2.
+
+
 
 
